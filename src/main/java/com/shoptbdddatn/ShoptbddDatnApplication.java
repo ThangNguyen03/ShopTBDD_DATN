23c2375b9379c9a67f70ce807de9864225297f84
Thang Nguyen <doivaigay84@gmail.com>
2023-04-11 17:06:16 +0700

    Tao securityConfig de chay cac api

--- src/main/java/com/shoptbdddatn/ShoptbddDatnApplication.java
+++ src/main/java/com/shoptbdddatn/ShoptbddDatnApplication.java
@@ -2,8 +2,10 @@ package com.shoptbdddatn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShoptbddDatnApplication {
 
 	public static void main(String[] args) {
